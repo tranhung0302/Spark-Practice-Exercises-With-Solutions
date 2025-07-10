5a72a25aeca30505744bc672db5700a30160a44a
Hung Tran <hungtranthanh0302@gmail.com>
2025-07-10 22:48:04 +0700

    more solution forexercise 1

--- excercises/1/code.py
+++ excercises/1/code.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import expr, udf
+from pyspark.sql.functions import expr, udf, array_remove
 from pyspark.sql.types import StringType, ArrayType
 
 spark = SparkSession.builder.appName("SparkPractice").getOrCreate()
@@ -21,5 +21,7 @@ udf_split = udf(custom_split, ArrayType(StringType()))
 result = df.withColumn("split_values", udf_split("values", "delimiter"))
 result.show(truncate=False)
 
-extra = result.withColumn("extra", expr("filter(split_values, x -> x != '')"))
+extra = result.withColumn("extra", array_remove(result.split_values, ""))
+# alternatively
+# extra = result.withColumn("extra", expr("filter(split_values, x -> x != '')"))
 extra.show(truncate=False)
